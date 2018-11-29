0d8d85ee127870a7a2deebb84145b02d7ac3bbdd
Boyman10 <contact@yasi.fr>
2018-11-29 22:07:33 +0100

    on zuul..

--- bookmicro/src/main/java/com/rhm/book/bookmicro/BookmicroApplication.java
+++ bookmicro/src/main/java/com/rhm/book/bookmicro/BookmicroApplication.java
@@ -2,9 +2,22 @@ package com.rhm.book.bookmicro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BookmicroApplication {
+
+    @RequestMapping(value = "/available")
+    public String available() {
+        return "Spring in Action";
+    }
+
+    @RequestMapping(value = "/checked-out")
+    public String checkedOut() {
+        return "Spring Boot in Action";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BookmicroApplication.class, args);
--- zuuledge/src/main/java/com/rhm/zuuledge/zuuledge/ZuuledgeApplication.java
+++ zuuledge/src/main/java/com/rhm/zuuledge/zuuledge/ZuuledgeApplication.java
@@ -2,8 +2,10 @@ package com.rhm.zuuledge.zuuledge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ZuuledgeApplication {
 
     public static void main(String[] args) {
