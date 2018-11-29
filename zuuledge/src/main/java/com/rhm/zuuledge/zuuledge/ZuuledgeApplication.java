8bab10e36b1cb4cf81f2c13fa20c91c3841cbb97
Boyman10 <contact@yasi.fr>
2018-11-29 21:53:38 +0100

    Preparing edge service and book micro app to test the edge service running Zuul.

---
+++ zuuledge/src/main/java/com/rhm/zuuledge/zuuledge/ZuuledgeApplication.java
@@ -0,0 +1,12 @@
+package com.rhm.zuuledge.zuuledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuuledgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuuledgeApplication.class, args);
+    }
+}
